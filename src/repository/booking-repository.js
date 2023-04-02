6261fa152bb5580c0802aa15bb89b70c27f011cc
ritik <gupta.ritik2002@gmail.com>
2023-04-02 15:32:16 +0530

    Added create booking repository code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,29 @@
+const { Booking } = require("../models/booking");
+const { AppError, ValidationError } = require("../utils/errors/index");
+const { StatusCodes } = require("http-status-codes");
+
+class BookingRepostiory {
+  async create(data) {
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      throw new AppError(
+        "RepositoryError",
+        "Cannot create Booking",
+        "There was some issue creating the booking,please try again later ",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async update(data){
+    
+  }
+
+}
+
+module.exports = BookingRepostiory;
