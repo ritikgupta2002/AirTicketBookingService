590bc5cd5fd0b3f134ba7d133ca1640cf4b1f905
ritik <gupta.ritik2002@gmail.com>
2023-04-02 14:44:04 +0530

    Added error files

---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,14 @@
+const { StatusCodes } = require("http-status-codes");
+class ValidationError extends Error {
+  constructor(error) {
+    super();
+    let explantion = [];
+    error.errors.foreach((err) => {
+      explantion.push(err.message);
+    });
+    (this.name = "Validation Error"),
+      (this.message = "Not able to validate the data sent in the request"),
+      (this.explanation = explanation),
+      (this.statusCode = StatusCodes.BAD_REQUEST);
+  }
+}
